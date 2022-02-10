b5fe11424b09a2097dd59d6afb2e3d73bf94686b
Uchchash346 <mru416@gmail.com>
2022-02-10 10:33:09 +0600

    function and static keyword

--- index.php
+++ index.php
@@ -9,14 +9,43 @@
 <body>
     <?php 
     // Variables
-    $studentName = "Mahmudur Rahman";
-    $studentID = "181-15-11276";
-    $cgpa = "3.72";
-    $university = "Daffodil INternational UNiversity";
+    // $studentName = "Mahmudur Rahman";
+    // $studentID = "181-15-11276";
+    // $cgpa = "3.72";
+    // $university = "Daffodil INternational UNiversity";
+    // echo "My name is: $studentName . My ID is $studentID and I am from $university" ; 
+    // echo "<br>";
+    // echo "CGPA: $cgpa";
 
-    echo "My name is: $studentName . My ID is $studentID and I am from $university" ; 
-    echo "<br>";
-    echo "CGPA: $cgpa";
+    // Function in PHP
+    // For global scope
+    $result = 3.92;
+    function myResult(){
+        echo "<h1>I got CGPA $result</h1>"; // It shows error
+    }
+    myResult();
+    echo "<h2>Mamur beta ami $result paisi. Yahoo!</h2>";
+
+    // For Local scope
+    function busFair(){
+        $localFair = 50; 
+        echo "<h2>Thikana bus er vara $localFair Taka.</h2>";
+    }
+    busFair();
+
+    // Static keyword
+    function myTest() {
+        static $x = 0;
+        echo $x;
+        $x++;
+      }
+      
+      myTest();
+      echo "<br>";
+      myTest();
+      echo "<br>";
+      myTest();
+
     ?>
 </body>
 </html>

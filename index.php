3d2fa26de3cc814013d69a0efb3875109c44a60b
Uchchash346 <mru416@gmail.com>
2022-02-10 10:05:20 +0600

    variables in php

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP DOCUMENT</title>
 </head>
 <body>
-    Hello PHP
+    <?php 
+    $studentName = "Mahmudur Rahman";
+    $studentID = "181-15-11276";
+    $cgpa = "3.72";
+    $university = "Daffodil INternational UNiversity";
+
+    echo "My name is: $studentName . My ID is $studentID and I am from $university" ; 
+    echo "<br>";
+    echo "CGPA: $cgpa";
+    ?>
 </body>
 </html>
